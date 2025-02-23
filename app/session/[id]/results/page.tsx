5d23029d376a50c80418e0269de9a3d6addf9379
Gianlorenzo Occhipinti <gianlorenzo.occhipinti@gmail.com>
2025-02-23 19:05:40 +0100

    refactor: rename 'Votes' column to 'Total Score' for clarity in results table

--- app/session/[id]/results/page.tsx
+++ app/session/[id]/results/page.tsx
@@ -191,7 +191,7 @@ export default function ResultsPage() {
                 <TableHeader>
                   <TableColumn>Avatar</TableColumn>
                   <TableColumn>Name</TableColumn>
-                  <TableColumn>Votes</TableColumn>
+                  <TableColumn>Total Score</TableColumn>
                 </TableHeader>
                 <TableBody>
                   {sortedGuests.map((guest) => {
